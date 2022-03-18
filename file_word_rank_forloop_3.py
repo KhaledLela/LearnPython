17f6913d7fed3823483893e4b98a302e1ff1d617
Khaled Lela <eng.khaled.lela@gmail.com>
2022-03-19 01:24:47 +0200

    add if condition example, and forloop example

---
+++ file_word_rank_forloop_3.py
@@ -0,0 +1,31 @@
+# py4e.com
+# File words rank...
+# نطلب من المستخدم إدخال اسم الملف او المسار
+# عدة سطور
+# البرنامج يقرأ سطر بسطر
+# نقرأ كلمة بكلمة من كل سطر
+# مخزن عبارة عن فهرس
+# كل كلمة - مقابل عدد مرات التكرار
+# من خلال الفهرس نبحث على اكثر الكلمات تكرارا
+# وعدد مرات تكررها
+
+filename = input('Please enter file name/path:\n') # أدخل اسم الملف او المسار
+lines = open(filename, 'r') # r = read  للقراءة  & w = write للكتابة
+
+w_counts = dict() # الفهرس بتاع التكرار
+for line in lines: # تكرار على السطور داخل الملف
+    words = line.split() # كل سطر بقوم بتحويلة لكلمات
+    for word in words:
+        w_counts[word] = w_counts.get(word, 0) + 1
+
+print(w_counts)
+
+bw = None # اكثر كلمة تكررا
+bc = None # اكبر عدد تكرار
+
+for word, count in list(w_counts.items()):
+    if bc is None or bc < count:
+        bc = count
+        bw = word
+
+print(bw,bc)
