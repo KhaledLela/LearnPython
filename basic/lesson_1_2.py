bd454934421baa165e65aeb21e99b236318b87a8
Khaled Lela <eng.khaled.lela@gmail.com>
2022-09-26 10:34:39 +0200

    update if conditional examples with comments

--- basic/lesson_1_2.py
+++ basic/lesson_1_2.py
@@ -7,3 +7,5 @@ if x % 2 == 0:
         print('Divisible by 2 and not by 3')
 elif x % 3 == 0:
     print('Divisible by 3 and not by 2')
+else:
+    print('Not divisible by 3 and not by 2')
--- basic/lesson_2.py
+++ basic/lesson_2.py
@@ -1,4 +1,10 @@
 x = int(input('Enter your age:\n'))
+
+"""
+This is a comment
+written in
+multi lines,
+"""
 print("You entered: " * x)
 if x % 2 == 0:
     print("Even number:")
@@ -6,6 +12,10 @@ if x % 2 == 0:
     print("3rd line")
 elif x > 5:
     print("More than 5")
+
+# This is a comments, not part of code
+# Only for documentation.
+# شرح تفاضيل ومعلومات عن الكود
 # elif 5 < x < 10:
 elif x > 5 and x < 10:
     print("Between 5-10")
