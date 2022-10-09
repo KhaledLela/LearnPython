e9b510ea8de107aec2a45384f8eae4317ffddf20
Khaled Lela <eng.khaled.lela@gmail.com>
2022-10-10 00:01:09 +0200

    add lesson 7 example, and try expect

--- basic/lesson_6.py
+++ basic/lesson_6.py
@@ -1,23 +1,31 @@
-# Exercise 1: Write a program which repeatedly reads numbers
-# until the user enters “done”. Once “done” is entered,
-# print out the total, count, and average of the numbers.
-# If the user enters anything other than a number,
-# detect their mistake using try and except and print an error message
-# and skip to the next number.
-
+"""
+Exercise 1: Write a program which repeatedly reads numbers
+ until the user enters “done”. Once “done” is entered,
+ print out the total, count, and average of the numbers.
+ If the user enters anything other than a number,
+ detect their mistake using try and except and print an error message
+ and skip to the next number.
+"""
 
 # total, count, average
 # average = total / count
 # Invalid div 0/0
+
+"""
+Value user input
+When user enter done stop
+"""
 value = None
 total = 0
 count = 0
 while value != 'done':
     value = input("Enter number:\n")
     if value != 'done':
-        total += int(value)
-        count += 1
-
+        try:
+            total += int(value)
+            count += 1
+        except ValueError:
+            print("Error:: Should be number or done")
 print("Total= " + str(total))
 print("Count= " + str(count))
 print("average= " + str(total/count))
