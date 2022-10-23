97bfaec36169a31042af7f0ea1c5fe8f9b0fdd83
Abotalhaa <as7109464@gmail.com>
2022-10-23 21:50:20 +0200

    إضافه الاختبار الخامس

--- basic/lesson_1_1.py
+++ basic/lesson_1_1.py
@@ -1,5 +1,5 @@
 x = int(input('Enter an integer: '))
-if x % 2 == 0:
+if x % 2 >= 0:
     print('')
     print('Even')
 else:
--- basic/lesson_9_3.py
+++ basic/lesson_9_3.py
@@ -6,5 +6,7 @@ str4 = str1 + str3
 print(str3[:-1])
 print(str1[1:])
 print(str4[1:9])
-print(str4[1:9:2])
-print(str4[::-1])
+print(str4[1:8:2])
+print(str4[::3])
+
+
