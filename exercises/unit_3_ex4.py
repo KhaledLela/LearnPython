b2ed5c8ee6c74f2723ba301cf441bb5a0ad2c3cd
Khaled Lela <eng.khaled.lela@gmail.com>
2022-11-15 13:11:57 +0200

    prime number exercise

--- exercises/unit_3_ex4.py
+++ exercises/unit_3_ex4.py
@@ -1,10 +1,4 @@
 """
-https://en.wikipedia.org/wiki/Prime_number
-https://ar.wikipedia.org/wiki/عدد_أولي
-Write a program ask user for input
-a number and print if this number is a prime number or not.
-ex. 5
-5 is a prime number.
-ex. 9
-9 is not a prime number.
+Write a program ask user for input two values
+v1 & v2 and print larger value
 """
