31267fbcc5c762f2ad1892378652a2669f292516
european <am.lela2006@gmail.com>
2022-11-18 21:13:03 +0200

    amlela

--- exams/amlela/unit_3_ex_3.py
+++ exams/amlela/unit_3_ex_3.py
@@ -6,3 +6,14 @@ Then go throw each word to
 2. print word that repeated more times
 """
 s = input('Enter a sentence:\n')
+w_counts = dict()
+
+words =s.split()
+for word in words:
+    w_counts[word] = w_counts.get(word, 0) + 1
+
+print(w_counts)
+print("repetition count", len(words))
+
+
++9*-
