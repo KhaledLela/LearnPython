eba987525f469a3e73611391c5c0ec55c86f28c7
Abotalhaa <as7109464@gmail.com>
2022-11-20 22:50:11 +0200

    'إضافة '

--- exercises/Abotalhaa/lessons/lesson_1_1.py
+++ exercises/Abotalhaa/lessons/lesson_1_1.py
@@ -1,5 +1,8 @@
-
-
-
-
+num = int(input('Enter number:\n'))
+for i in range(2, num):
+    if num % i == 0:
+        print(' Not a number prime')
+        break
+else:
+    print('prime number  ' + '\"' + str(num) + '\"')
 
