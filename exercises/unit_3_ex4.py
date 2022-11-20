10cb8e5f96e3e494427ff3df596ae4dbd2697348
Ahmed Hamdy <ahmedhamdy7796@gmail.com>
2022-11-20 21:11:45 +0200

    Merge branch 'main' of https://github.com/KhaledLela/LearnPython into ah_code

--- exercises/unit_3_ex4.py
+++ exercises/unit_3_ex4.py
@@ -1,4 +1,12 @@
-"""
-Write a program ask user for input two values
-v1 & v2 and print larger value
-"""
+# """
+# Write a program ask user for input two values
+# v1 & v2 and print larger value
+# """
+v1 = int(input("enter a number:\n"))
+v2 = int(input("enter a number:\n"))
+if v1 > v2:
+    print(v1, "is the large value")
+elif v1 < v2:
+    print(v2, "is the larger value")
+else:
+    print("the values are equals")
