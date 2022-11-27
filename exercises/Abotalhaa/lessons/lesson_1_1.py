2e7373db65b9a19a7bfea896630c2da8e3be95d7
Abotalhaa <as7109464@gmail.com>
2022-11-27 20:22:11 +0200

    'إضافة '

--- exercises/Abotalhaa/lessons/lesson_1_1.py
+++ exercises/Abotalhaa/lessons/lesson_1_1.py
@@ -1,10 +1,10 @@
 num = int(input('Enter number:\n'))
 if num < 2:
-    print('Not a prime number')
+    print('Not a prime number ' + '\"' + str(num) + '\"')
 else:
     for i in range(2, num):
         if num % i == 0:
-            print('Not a prime number')
+            print('Not a prime number ' + '\"' + str(num) + '\"')
             break
     else:
         print('prime number ' + '\"' + str(num) + '\"')
