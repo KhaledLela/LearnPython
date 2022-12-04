468356f8bbec16533f1babfc7a65536c1d8d8a51
european <am.lela2006@gmail.com>
2022-12-04 20:11:40 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	exams/amlela/unit_3_ex_1.py
    #	exams/unit_3_ex_2.py
    #	exercises/unit_3_ex3.py
    #	exercises/unit_3_ex4.py

---
+++ team/amlela/unit_3_ex7.py
@@ -0,0 +1,13 @@
+"""
+Write a python program to print the factorial of a given number
+https://en.wikipedia.org/wiki/Factorial
+https://ar.wikipedia.org/wiki/عاملي
+"""
+n = int(input("Enter the number: "))
+factorial = 1
+# write you code
+# here...
+
+for n in range(1, n):
+    if x * n == 1:
+         print("Factorial of", n, "is:", factorial)
