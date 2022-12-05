950c81f5ca635feb791954af6147cee046b2f37c
Khaled Lela <eng.khaled.lela@gmail.com>
2022-12-05 14:09:32 +0200

    exercise of two-sum

--- exercises/unit_4_ex1.py
+++ exercises/unit_4_ex1.py
@@ -6,25 +6,22 @@ https://leetcode.com/problems/two-sum
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 # You can return the answer in any order.
 
-Example 1:
-
+# Example 1:
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-Example 2:
 
+# Example 2:
 Input: nums = [3,2,4], target = 6
 Output: [1,2]
-Example 3:
 
+# Example 3:
 Input: nums = [3,3], target = 6
 Output: [0,1]
 
-
 Constraints:
-
-2 <= nums.length <= 104
--109 <= nums[i] <= 109
--109 <= target <= 109
+2 <= nums.length <= 10ˆ4
+-10ˆ9 <= nums[i] <= 10ˆ9
+-10ˆ9 <= target <= 10ˆ9
 Only one valid answer exists.
 """
