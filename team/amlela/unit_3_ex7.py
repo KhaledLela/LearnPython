5c97b0ecd10b2aaf6378d83c1a2baadd573e06ff
amlela2006 <115741400+amlela2006@users.noreply.github.com>
2022-12-05 10:31:52 +0200

    solve exercise  (#12)
    * Merge remote-tracking branch 'origin/main'
    
    # Conflicts:
    #	exams/amlela/unit_3_ex_1.py
    #	exams/unit_3_ex_2.py
    #	exercises/unit_3_ex3.py
    #	exercises/unit_3_ex4.py
    
    * Merge remote-tracking branch 'origin/main'
    
    # Conflicts:
    #	exams/amlela/unit_3_ex_1.py
    #	exams/unit_3_ex_2.py
    #	exercises/unit_3_ex3.py
    #	exercises/unit_3_ex4.py
    
    * Merge remote-tracking branch 'origin/main'
    
    # Conflicts:
    #	exams/amlela/unit_3_ex_1.py
    #	exams/unit_3_ex_2.py
    #	exercises/unit_3_ex3.py
    #	exercises/unit_3_ex4.py
    
    * Merge remote-tracking branch 'origin/main'
    
    # Conflicts:
    #	exams/amlela/unit_3_ex_1.py
    #	exams/unit_3_ex_2.py
    #	exercises/unit_3_ex3.py
    #	exercises/unit_3_ex4.py
    
    * Merge remote-tracking branch 'origin/main'
    
    # Conflicts:
    #	exams/amlela/unit_3_ex_1.py
    #	exams/unit_3_ex_2.py
    #	exercises/unit_3_ex3.py
    #	exercises/unit_3_ex4.py

---
+++ team/amlela/unit_3_ex7.py
@@ -0,0 +1,10 @@
+"""
+Write a python program to print the factorial of a given number
+https://en.wikipedia.org/wiki/Factorial
+https://ar.wikipedia.org/wiki/عاملي
+"""
+n = int(input("Enter the number: "))
+factorial = 1
+# write you code
+# here...
+
