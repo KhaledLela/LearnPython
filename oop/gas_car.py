644443694d9943361438bcce74095a7e18b7e9d5
Khaled Lela <eng.khaled.lela@gmail.com>
2023-04-07 07:31:47 +0200

    oop inheritance Exercise: Create a GasCar class that inherits from the Car

---
+++ oop/gas_car.py
@@ -0,0 +1,14 @@
+"""
+Exercise:
+Create a GasCar class that inherits from the Car class and has an additional attribute:
+
+fuel_type (string): the type of fuel the car uses
+The GasCar class should have a  refuel() method that prints the message "The gas tank has been refueled."
+
+
+"""
+from car import Car
+
+
+class GasCar(Car):
+    pass
