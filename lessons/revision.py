239e088d3d3f1d6879bb94a7ba478aa1b7fd903d
Khaled Lela <eng.khaled.lela@gmail.com>
2023-05-02 00:02:42 +0300

    revision for ML

---
+++ lessons/revision.py
@@ -0,0 +1,23 @@
+# print("Mohammed Lela")
+# print("Mohammed Lela")
+# print("Mohammed Lela")
+
+# name = "Ahmed"
+# name = input("Enter your name:\n")
+#
+# print("He is " + name) # He is Mohammed
+# print(name + " is 15 years old")
+# print(name + " From Egypt")
+
+# x = 10
+# y = 20
+# z = x + y # 30
+# print("sum = " + str(z))
+# print("sum =",z)
+
+age = input("Enter your age:\n")
+age = int(age)
+if age >= 15:
+    print("Can register!")
+else:
+    print("Error, Not valid for register!")
