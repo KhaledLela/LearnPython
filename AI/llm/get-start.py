2ca982b6e649839747e2e3daf560b9ee77673288
Khaled Lela <eng.khaled.lela@gmail.com>
2023-05-15 00:34:01 +0300

    whisper transcribe

--- AI/llm/get-start.py
+++ AI/llm/get-start.py
@@ -25,7 +25,14 @@ deactivate
 """
 from dotenv import load_dotenv
 import os
-from langchain.llms import OpenAI
+
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.chains.summarize import load_summarize_chain
+from langchain import OpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.docstore.document import Document
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains.summarize import load_summarize_chain
 
 # Load environment variables from .env file
 load_dotenv()
@@ -33,6 +40,20 @@ os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
 
 
 llm = OpenAI(temperature=0.9)
+
+loader = UnstructuredFileLoader("/Users/khaledlela/Downloads/test.txt")
+document = loader.load()
+
+char_text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+docs = char_text_splitter.split_documents(document)
+
+model = load_summarize_chain(llm=llm, chain_type="refine")
+model.run(docs)
+
+print(len(docs))
+
+for doc in docs:
+    print(doc)
 prompt = "What the main language for GPT?"
 print(llm(prompt))
 # The main language used by GPT (Generative Pre-trained Transformer) is Python.
