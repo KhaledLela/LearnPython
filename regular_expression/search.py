12b8a96e0ecf4cf4a116d102e2ad50d4f2639bb1
Khaled Lela <eng.khaled.lela@gmail.com>
2023-06-12 00:21:42 +0300

    Search re

--- regular_expression/search.py
+++ regular_expression/search.py
@@ -1,7 +1,11 @@
 # Search for lines that contain 'From'
 import re
-hand = open('mbox-short.txt')
-for line in hand:
-    line = line.rstrip()
-    if re.search('From:', line):
-        print(line)
+
+with open('mbox-short.txt') as hand:
+    for line in hand:
+        line = line.rstrip()
+        # if line.find('From:') != -1:
+        # if re.search('From:', line):
+        # if line.startswith('From:'):
+        if re.search('^From:.+@', line):
+            print(line)
