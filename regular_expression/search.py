58dff78571ca0fa0e4704eed7ab5f37018411c63
Khaled Lela <eng.khaled.lela@gmail.com>
2023-06-12 11:40:19 +0300

    Search re references

--- regular_expression/search.py
+++ regular_expression/search.py
@@ -1,3 +1,27 @@
+"""
+https://www.py4e.com/html3/11-regex
+https://www.py4e.com/lectures3/Pythonlearn-11-Regex-Handout.txt
+
+Python Regular Expression Quick Guide
+
+^        Matches the beginning of a line
+$        Matches the end of the line
+.        Matches any character
+\s       Matches whitespace
+\S       Matches any non-whitespace character
+*        Repeats a character zero or more times
+*?       Repeats a character zero or more times
+         (non-greedy)
++        Repeats a character one or more times
++?       Repeats a character one or more times
+         (non-greedy)
+[aeiou]  Matches a single character in the listed set
+[^XYZ]   Matches a single character not in the listed set
+[a-z0-9] The set of characters can include a range
+(        Indicates where string extraction is to start
+)        Indicates where string extraction is to end
+"""
+
 # Search for lines that contain 'From'
 import re
 
